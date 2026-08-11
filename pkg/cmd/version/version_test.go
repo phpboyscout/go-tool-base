43753f9a1f6a95aa69b0be501100caa113005c20
Matt Cockayne <matt@mattcockayne.uk>
2026-08-11 00:08:01 +0000

    fix(deps): update module github.com/google/go-github/v89 to v90

--- pkg/cmd/version/version_test.go
+++ pkg/cmd/version/version_test.go
@@ -8,7 +8,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/google/go-github/v89/github"
+	"github.com/google/go-github/v90/github"
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
